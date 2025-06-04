4d9b039a64141ae8be95dec0f1b320ed3d123950
kimwat7601 <kimwat7601@gmail.com>
2025-06-04 22:28:02 +0900

    連続検索でのキャンセル処理実装

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ import { setDaysData } from '@/processor/setForecastWeatherData';
 const App = () => {
   const [weatherData, setWeatherData] = useState<CurrentWeatherData | null>(null);
   const [forecastData, setForecastData] = useState<ForecastWeatherData[][] | null>(null);
-  const [isCureentLoading, setIsCurrentLoading] = useState(false);
+  const [isCurrentLoading, setIsCurrentLoading] = useState(false);
   const [isForecastLoading, setIsForecastLoading] = useState(false);
   const [currentError, setCurrentError] = useState('');
   const [forecastError, setForecastError] = useState('');
@@ -30,6 +30,9 @@ const App = () => {
   useEffect(() => {
     if (!searchCity.trim()) return;
 
+    const controller = new AbortController();
+    const signal = controller.signal;
+
     const fetchCurrentWeather = async () => {
       setIsCurrentLoading(true);
       setCurrentError('');
@@ -37,12 +40,16 @@ const App = () => {
         const currentData = await fetchData<CurrentWeatherFetchData>(
           searchCity,
           import.meta.env['VITE_OPEN_WEATHER_API_KEY'],
-          'weather'
+          'weather',
+          { signal: signal }
         );
         const newWeatherData: CurrentWeatherData =
           currentData && setCurrentWeatherInfo(currentData);
         setWeatherData(newWeatherData);
       } catch (error) {
+        if (error instanceof Error && error.name === 'AbortError') {
+          return;
+        }
         if (error instanceof Error) {
           setCurrentError(error.message);
         } else {
@@ -53,10 +60,17 @@ const App = () => {
       }
     };
     fetchCurrentWeather();
+
+    return () => {
+      controller.abort();
+    };
   }, [searchCity]);
 
   useEffect(() => {
     if (!searchCity.trim()) return;
+
+    const controller = new AbortController();
+    const signal = controller.signal;
 
     const fetchForecastWeather = async () => {
       setIsForecastLoading(true);
@@ -65,12 +79,16 @@ const App = () => {
         const forecastWeatherdata = await fetchData<ForecastWeatherFetchData>(
           searchCity,
           import.meta.env['VITE_OPEN_WEATHER_API_KEY'],
-          'forecast'
+          'forecast',
+          { signal: signal }
         );
         const newForecastData: ForecastWeatherData[][] =
           forecastWeatherdata && setDaysData(forecastWeatherdata);
         setForecastData(newForecastData);
       } catch (error) {
+        if (error instanceof Error && error.name === 'AbortError') {
+          return;
+        }
         if (error instanceof Error) {
           setForecastError(error.message);
         } else {
@@ -81,6 +99,9 @@ const App = () => {
       }
     };
     fetchForecastWeather();
+    return () => {
+      controller.abort();
+    };
   }, [searchCity]);
 
   return (
@@ -89,7 +110,7 @@ const App = () => {
         <SearchForm handleSearch={handleSearch} />
       </section>
       <section className="dashboard-body">
-        <CurrentWeather data={weatherData} isLoading={isCureentLoading} error={currentError} />
+        <CurrentWeather data={weatherData} isLoading={isCurrentLoading} error={currentError} />
         <ForecastWeatherList
           data={forecastData}
           isLoading={isForecastLoading}
